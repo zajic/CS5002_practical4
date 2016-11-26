32dc2179321a3afac6dd6bdd9e4141474d71536f
huangrenkun <rh97@st-andrews.ac.uk>
2016-11-26 17:25:10 +0000

    11-26 input

--- guardian_reader.js
+++ guardian_reader.js
@@ -18,22 +18,22 @@ function userInput(searchWord,tag,fromDate,toDate,mediaType,contributor,sortedBy
 		keyword:tag,
 		author:contributor
 	};
-	
+
 	this.fromDate= {
 		query:"&from-date=",
 		value:fromDate
 	};
-	
+
 	this.toDate={
 		query:"&to-date=",
 		value:toDate,
 	};
-	
+
 	this.mediaType={
 		query:"&type=",
 		value:mediaType
 	};
-		
+
 	this.sortedBy={
 		query:"&order-by=",
 		value:sortedBy
@@ -43,7 +43,7 @@ function userInput(searchWord,tag,fromDate,toDate,mediaType,contributor,sortedBy
 
 //generate a query from inputs stored in userInput object
 userInput.prototype.generateQuery = function() {
-	
+
 	var urlQuery="";
 	//go through the userInput properties and if a property has a value, add it to the urlQuery (tags are handled separately)
 	for (prop in this) {
@@ -52,7 +52,7 @@ userInput.prototype.generateQuery = function() {
 				urlQuery=urlQuery + this[prop].query + this[prop].value
 		}
 	}
-	
+
 	//create the "tag" part of the query: by default the tag query only contains the "us-news/us-elections-2016" tag
 	var tags=this.tags.query;
 	//if there are any other tags specified they are added and separated by comma
@@ -77,7 +77,7 @@ function getArticles() {
 
 //add the script with jsonp url to the html
 function executeJsonp(/*string*/url) {
-	
+
 	console.log(url);
 	var scriptElement = document.createElement("script");
 	scriptElement.src = url;
@@ -88,13 +88,13 @@ function executeJsonp(/*string*/url) {
 
 //create url for the request: the url consists of static query part that never change and dynamic query generated based on user input
 function createUrl(/*object*/userInput) {
-	
+
 	if (typeof userInput == 'object' && arguments.length>0) {
 		//the basic part of url is the same for every request
 		var baseurl = "https://content.guardianapis.com/search"
 		var APIkey="?api-key=cd76717d-271b-47b9-a69b-c06e83a77405"
 		var staticOptions="&show-fields=thumbnail,trailText&show-tags=keyword,contributor&page-size=50&format=json&callback=callback"
-		
+
 		//generate the dynamic part of the query
 		var userQueryOptions=userInput.generateQuery();
 
@@ -107,7 +107,7 @@ function createUrl(/*object*/userInput) {
 
 //callback function used in JSONP request
 function callback(/*object*/data) {
-	
+
 	//cleanup the previous jsonp request
 	cleanupScript();
 	console.log(data);
@@ -126,13 +126,13 @@ function callback(/*object*/data) {
 
 //removes previous jsonp requests
 function cleanupScript() {
-	
+
 	var scriptElement = document.getElementById("jsonp");
 	scriptElement.parentNode.removeChild(scriptElement);
 }
 
 function createTiles() {
-	
+
 			var newHeader1=news[0].webTitle;
 			var textBody1=(news[0].fields.trailText)
 			var thumbImg1=news[0].fields.thumbnail;
@@ -142,18 +142,18 @@ function createTiles() {
 				if (news[0]["tags"][x]["type"] == "contributor")
 				var contributor1=news[0].tags[x].webTitle;
 			}
-			
+
 			document.getElementById("newsHeading1").innerHTML=newHeader1;
 			document.getElementById("newsBody1").innerHTML=textBody1;
 			document.getElementById("newsThumbnail1").src=thumbImg1;
 			document.getElementById("link1").href=link1;
 			document.getElementById("datePublished1").innerHTML="Published: " + datePub1.slice(0,10) + " " + datePub1.slice(-9,-1);
 			document.getElementById("contributedBy1").innerHTML="By " + contributor1;
-			
-			
+
+
 			var newHeader2=news[1].webTitle;
 			//var textBody2=(news[1].blocks.body[0].bodyTextSummary).slice(0,350) + "...";
-			var textBody2=(news[1].fields.trailText)
+			var textBody2=(news[1].fields.trailText)s
 			var thumbImg2=news[1].fields.thumbnail;
 			var link2=news[1].webUrl;
 			var datePub2=news[1].webPublicationDate;
@@ -169,4 +169,3 @@ function createTiles() {
 			document.getElementById("contributedBy2").innerHTML="By " + contributor2;
 
 }
-
--- input.js
+++ input.js
@@ -3,23 +3,47 @@ var searchWord;
 var advancedFlag = false;
 
 function init() {
+    wipeAll();
     document.getElementById("search").onclick = search;
     document.getElementById("advancedSearch").onclick = showAdvancedSearch;
-
 }
+
+//wipe all inputs when page refreshed
+function wipeAll(){
+  var searchWord = document.getElementById("searchWord");
+  searchWord.value="";
+  var operator = document.getElementById("operator");
+  operator.value="";
+  var secondSearchWord = document.getElementById("secondSearchWord");
+  secondSearchWord.value="";
+  var fromDate = document.getElementById("fromDate");
+  fromDate.value="";
+  var toDate = document.getElementById("toDate");
+  toDate.value="";
+  var mediaType = document.getElementById("mediaTypeChosen");
+  mediaType.value="";
+  var contributor = document.getElementById("contributor");
+  contributor.value="";
+  var sortBy = document.getElementById("sortMethod");
+  sortBy.value="";
+}
+
 //receiving users' inputs
 function search() {
     searchWord = document.getElementById("searchWord").value;
     //Then show the result on the page.
     if (advancedFlag) {
-        var tag = document.getElementById("tag").value;
+      //the operator is the dropdown menu (containing "and","or","not") displayed when user click advanced search button
+      //the secondSearchWord is the input form displayed when user click advanced search button to allow another search word input
+        var operator = document.getElementById("operator").value;
+        var secondSearchWord = document.getElementById("secondSearchWord").value;
         var fromDate = document.getElementById("fromDate").value;
         var toDate = document.getElementById("toDate").value;
         var mediaType = document.getElementById("mediaTypeChosen").value;
         var contributor = document.getElementById("contributor").value;
-        var sort = document.getElementById("sortMethod").value;
+        var sortBy = document.getElementById("sortMethod").value;
 
-        // generate url. Check each advanced variable before add them to the 
+        // generate url. Check each advanced variable before add them to the
         // url. If there is no value or it is default value for selection element, then don't add it to the url.
         // send request with all advanced variables
 
@@ -36,12 +60,13 @@ function showAdvancedSearch() {
     } else {
         document.getElementById("advancedSearchField").style.display = "none";
         document.getElementById("advancedSearch").value = "Show Advanced Search";
-        document.getElementById("tag").value = '';
+        document.getElementById("operator").value='none';
+        document.getElementById("secondSearchWord").value='';
         document.getElementById("toDate").value = '';
         document.getElementById("fromDate").value = '';
         document.getElementById("mediaTypeChosen").value = 'none';
         document.getElementById("contributor").value = '';
-        document.getElementById("sortMethod").value = 'latestTenFirst';
+        document.getElementById("sortMethod").value = 'newest';
         advancedFlag = false;
     }
 
